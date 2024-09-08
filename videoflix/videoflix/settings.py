148c46a6b6ab004c4a469cce17232ab4b6a7d38d
Konrad Thiemann <konrad.thiemann@mdm.de>
2024-09-08 14:28:00 +0200

    create admin

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -116,9 +116,17 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflix',
+        'USER': 'videoflixuser',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
