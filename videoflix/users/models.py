53d6dadceff74a3580a0da58e674afe8bdcf5ff7
Konrad Thiemann <konrad.thiemann@mdm.de>
2024-09-16 10:49:14 +0200

    fix:same first and last name now possible

--- videoflix/users/models.py
+++ videoflix/users/models.py
@@ -33,8 +33,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=255)
     username = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=50, unique=True)
-    last_name = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
